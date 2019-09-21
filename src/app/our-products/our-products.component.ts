98c4baa1341151fb8f10a3d6251bd10d0bc45b91
unknown <suryaprakash.kumar@outlook.com>
2019-09-21 12:56:20 +0530

    new change images

--- src/app/our-products/our-products.component.ts
+++ src/app/our-products/our-products.component.ts
@@ -26,10 +26,10 @@ export class OurProductsComponent implements OnInit {
         items: 1
       },
       400: {
-        items: 2
+        items: 1
       },
       740: {
-        items: 3
+        items: 1
       },
       940: {
         items: 1
